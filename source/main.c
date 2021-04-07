08d1394a0f10c904aea77559c7cc1012419fe7ad
ryanchangx <ryanchangx@gmail.com>
2021-04-06 18:06:47 -0700

    Fixed some of exercise 4

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: rchan123
  *  	Partner(s) Name: 
  *	Lab Section: 023
- *	Assignment: Lab 2  Exercise 3
+ *	Assignment: Lab 2  Exercise 4
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -14,20 +14,22 @@
 
 int main(void) {
 	/* Insert DDR and PORT initializations */
-	DDRA = 0x00; 
-	PORTA = 0x00; 
-	DDRC = 0xFF;
-	PORTC = 0x00;
+	DDRA = 0x00; PORTA = 0x00;
+	DDRB = 0x00; PORTB = 0x00; 
+	DDRC = 0x00; PORTC = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
 	/* Insert your solution below */
-	// unsigned char tmpB = 0x00;
-	unsigned char tmpA = 0x00;
-	unsigned char cntavail = 0x00;
-	unsigned char allfull = 0x00;
+	unsigned char tmpD = 0x00;
+	unsigned char exceeds = 0x00;
+	unsigned char unbalanced = 0x00;
+	int total = 0;
 	while (1) {
-		tmpA = PINA & 0x0F;
-		cntavail = (1-((tmpA & 0x08) >> 3)) + (1-((tmpA & 0x04) >> 2)) + (1-((tmpA & 0x02) >> 1)) + (1-(tmpA & 0x01));
-		allfull = (cntavail == 0)? 0x80 : 0x00; 
-		PORTC = cntavail | allfull;
+		//calculate the total weight
+		total = PINA + PINB + PINC;
+		exceeds = (total > 140)? 0x01 : 0x00;	//weight exceeds 140, then 1 to PD0
+		unbalanced = ((PINA > 80 + PINC) || (PINC > 80 + PINA))? 0x02 : 0x00;
+		tmpD = (total >> 2); 
+		PORTD = tmpD | (exceeds | unbalanced);
 	}
 	return 0;
 }
--- turnin/rchan123_lab2_part4_tests.py
+++ turnin/rchan123_lab2_part4_tests.py
@@ -1,43 +1,28 @@
-/*	Author: rchan123
- *  	Partner(s) Name: 
- *	Lab Section: 023
- *	Assignment: Lab 2  Exercise 2
- *	Exercise Description: [optional - include for your own benefit]
- *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
- */
-#include <avr/io.h>
-#ifdef _SIMULATE_
-#include "simAVRHeader.h"
-#endif
+# Array of tests to run (in order)
+# Each test contains
+#   description - 
+#   steps - A list of steps to perform, each step can have
+#       inputs - A list of tuples for the inputs to apply at that step
+#       *time - The time (in ms) to wait before continuing to the next step 
+#           and before checking expected values for this step. The time should be a multiple of
+#           the period of the system
+#       *iterations - The number of clock ticks to wait (periods)
+#       expected - The expected value at the end of this step (after the "time" has elapsed.) 
+#           If this value is incorrect the test will fail early before completing.
+#       * only one of these should be used
+#   expected - The expected output (as a list of tuples) at the end of this test
+# An example set of tests is shown below. It is important to note that these tests are not "unit tests" in 
+# that they are not ran in isolation but in the order shown and the state of the device is not reset or 
+# altered in between executions (unless preconditions are used).
+tests = [ 
+    {'description': 'PINA: 0x00, PINB: 0x00, PINC: 0x00 => PORTD: 0x00',
+    'steps': [ {'inputs': [('PINA', 0x00), ('PINB', 0x00), ('PINC', 0x00)], 'iterations': 5 } ],
+    'expected': [('PORTD', 0x00)],
+    },
+    ]
 
-int main(void) {
-	/* Insert DDR and PORT initializations */
-	DDRA = 0x00; PORTA = 0x00;
-	DDRB = 0x00; PORTB = 0x00; 
-	DDRC = 0x00; PORTC = 0x00;
-	DDRD = 0xFF; PORTD = 0x00;
-	/* Insert your solution below */
-	unsigned char tmpD = 0x00;
-	int total = 0;
-	while (1) {
-		//calculate the total weight
-		tmpD = PINA + PINB + PINC;
-		if((tmpD >> 7) != 0){
-			tmpD = tmpD >> 2;
-		}
-		else if((tmpD >> 6) != 0){
-			tmpD = tmpD >> 1;
-		}
-		tmpD = tmpD << 2;
-		if(total > 140){
-			tmpD = tmpD | 0x01;
-		}
-		if(PORTA - PORTC > 80){
-			tmpD = tmpD | 0x02;
-		}
-		PORTD = tmpD;
-	}
-	return 0;
-}
+# Optionally you can add a set of "watch" variables these need to be global or static and may need
+# to be scoped at the function level (for static variables) if there are naming conflicts. The 
+# variables listed here will display everytime you hit (and stop at) a breakpoint
+# watch = ['PORTB']
+
